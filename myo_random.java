6cf7279ab02c10978cd49eb3df99f1372e8ebe08
1097315Aliya <1097315@pdsb.net>
2024-09-26 14:22:08 -0400

    Update myo_random.java

--- myo_random.java
+++ myo_random.java
@@ -6,3 +6,16 @@ class Testing {
     // your code ends here!
   }
 }
+class Testing {
+  public static void main(String [] args) {
+    // place your code here!
+    System.out.println("The number of keys on a keyboard is");
+    System.out.print(102);
+    System.out.println();
+    System.out.println("The value of PI is: ");
+    System.out.print(3.14);
+    System.out.println();
+
+    // your code ends here!
+  }
+}
